7a32b31df023db623b8394a4dcd36ff62c450932
ajjayaroraa <ajjayaroraa@gmail.com>
2020-03-17 10:46:14 +0530

    Update 2-DeclareVariables.py

--- 2-DeclareVariables.py
+++ 2-DeclareVariables.py
@@ -4,7 +4,7 @@
 # intentionally wipe out data through code
 
 fullName = "Ajjay Aroraa" # This is an example of how to store string type data
-someRandomNumber = "10" # This is an example of how to store integer type data
+someRandomNumber = 10 # This is an example of how to store integer type data
 is_val_true = False # This is an example of how to store boolean type data
 
 
@@ -16,3 +16,13 @@ print(is_val_true)
 # not with the number. Also, only letters, underscore, and numbers are allowed
 # Do note that variable names are case sensitive. So, FULLNAME is not same as
 # fullName or FullName or fullname and so on
+
+
+# If at any point of time you want to know what type of data is stored in a
+# variable, you can use below code
+
+print(type(fullName))
+print(type(someRandomNumber))
+print(type(is_val_true))
+
+# What it does is to print the type of data that is stored in a variable
