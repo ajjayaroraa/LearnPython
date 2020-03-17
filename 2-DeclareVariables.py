5fc5db7118f17de376afcde4ccd8f65aae884a05
ajjayaroraa <ajjayaroraa@gmail.com>
2020-03-17 10:36:09 +0530

    Create 2-DeclareVariables.py

---
+++ 2-DeclareVariables.py
@@ -0,0 +1,10 @@
+# In this code, we assign certain values to variables and the print them
+# Variables are the refrences to the physical storage in which code stores
+# the data on temporary basis i.e. till the program is running or we
+# intentionally wipe out data through code
+
+fullName = "Ajjay Aroraa" # This is an example of how to store string type data
+someRandomNumber = "10" # This is an example of how to store integer type data
+
+print(fullName)
+print(someRandomNumber)
