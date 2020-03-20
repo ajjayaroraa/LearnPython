5e35430ce5ac546e7988961561c0b965fb94a169
ajjayaroraa <ajjayaroraa@gmail.com>
2020-03-20 21:28:56 +0530

    20

---
+++ 5-ArithmeticOperations.py
@@ -0,0 +1,24 @@
+# Arithmetic Operations
+# Addition
+a = 5+3
+print(a)
+
+#Substration
+b = 9-2
+print(b)
+
+# Division
+c = 9/3
+print(c)
+
+# Multiplication
+d = 6*2
+print(d)
+
+# Exponent
+e = 2**3
+print(e)
+
+# Modulus
+f = 30%4
+print(f)
