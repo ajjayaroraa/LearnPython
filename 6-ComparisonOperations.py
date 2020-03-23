2a02dc92995d7c47dafa139ba860eacc8ce5864b
ajjayaroraa <ajjayaroraa@gmail.com>
2020-03-23 12:18:28 +0530

    23

---
+++ 6-ComparisonOperations.py
@@ -0,0 +1,21 @@
+# Declare variables
+num_1 = 10
+num_2 = 20
+
+# Lesser than
+print(num_1 < num_2)
+
+# Lesser than or equal to
+print(num_1 <= num_2)
+
+# Greater than
+print(num_1 > num_2)
+
+# Greater than or equal to
+print(num_1 >= num_2)
+
+# Equals to
+print(num_1 == num_2)
+
+# Not equal to
+print(num_1 != num_2)
