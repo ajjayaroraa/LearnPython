12261e0edc70d769d83d9da656ff6e1462ad11f1
ajjayaroraa <ajjayaroraa@gmail.com>
2020-03-23 13:34:12 +0530

    Update 7-LogicalOperations.py

--- 7-LogicalOperations.py
+++ 7-LogicalOperations.py
@@ -1,5 +1,16 @@
 # AND
 
+ans = (5>4) and (6>3)
+print(ans)
+ans = (5>4) and (6<3)
+print(ans)
+
 # OR
+ans = (5>4) or (6>3)
+print(ans)
+ans = (5>4) or (6<3)
+print(ans)
 
 # NOT
+ans = 5>4
+print(not ans)
