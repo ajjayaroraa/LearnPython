458514746d65d991889cb5315e5438234dcd0615
ajjayaroraa <ajjayaroraa@gmail.com>
2020-03-23 15:02:24 +0530

    Create 8-ConditionalStatements.py

---
+++ 8-ConditionalStatements.py
@@ -0,0 +1,17 @@
+# Syntax for if else
+
+num = 15
+
+# Example 1
+
+if num % 2 == 0:
+    print("even")
+else:
+    print("odd")
+
+# Example 2
+
+if num > 10:
+    print("greater than 10")
+else:
+    print("less than or equal to 10")
