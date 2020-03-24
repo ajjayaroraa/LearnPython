1dba9ebaf6fb5a9ad1d5ac5866101bbb213bf3e2
ajjayaroraa <ajjayaroraa@gmail.com>
2020-03-24 13:46:30 +0530

    Update 11-Lists.py

--- 11-Lists.py
+++ 11-Lists.py
@@ -26,3 +26,7 @@ print(sliced)
 # let start be blank if you wish to start from zero
 # let stop+1 be blank if you wish to let loop go through end
 # use negative indexing if you wish to start from last
+
+# get length or number of elements in a list
+
+print(len(numbers))
