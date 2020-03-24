3efd4c9e3b4eac25277d8a10b807665ec5ddb203
ajjayaroraa <ajjayaroraa@gmail.com>
2020-03-24 13:39:37 +0530

    Create 11-Lists.py

---
+++ 11-Lists.py
@@ -0,0 +1,20 @@
+# Lists in python
+
+numbers = [4,36,55,6,3,56,3,5,-5,5,-52,4,244]
+
+print(numbers)
+
+
+# Refer to an element in a list
+
+print(numbers[2])
+
+
+# iterate throught each element in list
+
+for x in numbers:
+    print(x)
+
+
+# slice a part of the list
+# name_of_list[start:stop+1]
