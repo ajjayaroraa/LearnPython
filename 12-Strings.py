d39dd24ed83591ebe8688303d98247fbe4ecaeb3
ajjayaroraa <ajjayaroraa@gmail.com>
2020-03-24 15:14:17 +0530

    Create 12-Strings.py

---
+++ 12-Strings.py
@@ -0,0 +1,17 @@
+# strings: string is a list of characters
+
+sample_str = "This is a sample string with 123456123@#@$$34"
+
+# print string
+print(sample_str)
+
+# print length of string
+print(len(sample_str))
+
+# print a single element of string
+print(sample_str[10])
+
+
+# part of string
+part_of_str = sample_str[5:23]
+print(part_of_str)
