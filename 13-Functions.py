bafedc8032fa780925d16d1d228e4aed2bf3d998
ajjayaroraa <ajjayaroraa@gmail.com>
2020-03-25 16:44:34 +0530

    Update 13-Functions.py

--- 13-Functions.py
+++ 13-Functions.py
@@ -5,3 +5,22 @@ def test_func():
     print("test")
 
 test_func()
+
+
+# Defining a function with parameters
+
+def one_more(num):
+    # do whatever you want to do
+    print(num*3)
+
+one_more(9)
+
+
+# Defining a function with parameters and return value
+
+def one_more_f(num):
+    # do whatever you want to do
+    return num*3
+
+ret_val = one_more_f(11)
+print(ret_val)
